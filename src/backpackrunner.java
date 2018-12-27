7ebd18776e7d74756265ae0880bcb572304d8d93
JoeJoePotato <league@WTS-iMac-11.attlocal.net>
2018-12-26 18:29:47 -0800

    backpack

--- src/backpackrunner.java
+++ src/backpackrunner.java
@@ -1,4 +1,13 @@
 
-public class backpackrunner extends Backpack {
-
+public class backpackrunner {
+public static void main(String[] args) {
+	Backpack b=new Backpack();
+	Pencil p=new Pencil();
+	b.putInBackpack(p);
+	Ruler r=new Ruler();
+	b.putInBackpack(r);
+	Textbook t=new Textbook();
+	b.putInBackpack(t);
+	b.goToSchool();
+}
 }
